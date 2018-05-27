b6e8cc6a77fa1c329472560778e196504a5bdae1
XuYiFanHHH <35620577+XuYiFanHHH@users.noreply.github.com>
2018-05-27 23:18:25 +0800

    Add files via upload

--- kbd.c
+++ kbd.c
@@ -3,19 +3,74 @@
 #include "defs.h"
 #include "kbd.h"
 
+extern void sendMessage(int wndId, PMessage msg);
+extern PWndList wndList;
+
+void kbdInterupt()
+{
+  static int shift = 0;
+  static uchar *charcode[2] = {
+    normalMap, e0Map
+  };
+
+  uint st, data;
+
+  st = inb(KBSTATP);
+  data = inb(KBDATAP);
+  if((st & KBS_DIB) == 0 || (st & 0x20) != 0)
+  {
+    //cprintf("kbdInterupt return : %d\n", st);
+    return;
+  }
+
+  if(data == 0xE0){
+    shift = 1;
+    return;
+  } else if(data & 0x80){
+    // Key released
+    data &= 0x7F;
+
+    PMessage msg;
+    msg.type = MSG_KEY_UP;
+    msg.param = charcode[shift][data];
+    if (shift)
+      shift = 0;
+    sendMessage(wndList.entry, msg);
+    return;
+  }
+  PMessage msg;
+  msg.type = MSG_KEY_DOWN;
+  msg.param = charcode[shift][data];
+  if (shift)
+    shift = 0;
+  sendMessage(wndList.entry, msg);
+}
+
 int
 kbdgetc(void)
 {
-  static uint shift;
+  static uchar shift;
   static uchar *charcode[4] = {
     normalmap, shiftmap, ctlmap, ctlmap
   };
   uint st, data, c;
 
   st = inb(KBSTATP);
-  if((st & KBS_DIB) == 0)
-    return -1;
   data = inb(KBDATAP);
+  if((st & KBS_DIB) == 0 || (st & 0x20) != 0)
+  {
+    //cprintf("kbdInterupt return : %d\n", st);
+    return -1;
+  }
+
+  if (st & 0xc0)
+  {
+    return -1;
+  }
+
+  //cprintf("kbdInterupt : %d\n", st);
+
+  //kbdInterupt(data);
 
   if(data == 0xE0){
     shift |= E0ESC;
@@ -46,5 +101,6 @@ kbdgetc(void)
 void
 kbdintr(void)
 {
-  consoleintr(kbdgetc);
+  //consoleintr(kbdgetc);
+  kbdInterupt();
 }
